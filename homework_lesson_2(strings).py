e9f5e94e5c799ca568940a5aa97540062bf76f5c
Andrew <andreym89@gmail.com>
2022-12-02 00:31:06 +0200

    Homework v1

---
+++ homework_lesson_2(strings).py
@@ -0,0 +1,77 @@
+# exc_3
+print('\t\timport antigravity\n\t\timport __hello__\n\t\tfrom __future__ import braces\n')
+
+# exc_4
+# line 1
+#divided by the line where each line is a single character
+print('\t\t*\t\t\t' \
+      '*\t\t* ' \
+      '********  \t' \
+      '********  \t' \
+      '********* ' \
+      '*\t\t  *')
+# line 2
+print('\t   * *\t\t\t' \
+      '**\t\t* ' \
+      '*\t\t   *\t' \
+      '*\t\t*\t' \
+      '*\t\t  ' \
+      ' *\t *')
+# line 3
+print('\t  *   *\t\t\t' \
+      '* *\t\t* '
+      '*\t\t\t*   ' \
+      '*\t\t*   ' \
+      '*\t\t  ' \
+      '  *\t*')
+# line 4
+print('\t *\t   *\t\t' \
+      '*  *\t* ' \
+      '*\t\t\t*   ' \
+      '*\t\t*   ' \
+      '*******   ' \
+      '\t * *')
+# line 5
+print('\t*\t\t*\t\t' \
+      '*\t*\t* ' \
+      '*\t\t\t*   ' \
+      '********  \t' \
+      '*\t\t  ' \
+      '    *')
+# line 6
+print('   ***********\t\t' \
+      '*\t *\t* ' \
+      '*\t\t\t*   ' \
+      '* *\t\t\t' \
+      '*\t\t  ' \
+      '\t *')
+# line 7
+print('  *\t\t\t  *\t\t' \
+      '*\t  *\t* ' \
+      '*\t\t\t*   ' \
+      '*   *\t\t' \
+      '*\t\t  ' \
+      '  *')
+# line 8
+print(' *\t\t\t   *\t' \
+      '*\t   ** ' \
+      '*\t\t   *\t' \
+      '*\t  *\t\t' \
+      '*\t\t  ' \
+      ' *')
+# line 9
+print('*\t\t\t\t*\t' \
+      '*\t\t* ' \
+      '********\t\t' \
+      '*\t\t*\t' \
+      '********* ' \
+      '*\n')
+
+# exc_5
+print('\\a\t\tBell (alert)')
+print('\\b\t\tBackspace')
+print('\\n\t\tNew line')
+print('\\t\t\tHorizontal tab')
+print('\\\\\t\tBackslash \\')
+print('\\\"\t\t\"Double quotation mark \"')
+print('\\\'\t\tSingle quotation mark \'')
