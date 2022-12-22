03259e487bf83164767164c683b6e79a108b2100
Andrew <andreym89@gmail.com>
2022-12-23 01:12:26 +0200

    Homework 5 task 1 - 6 done

--- homework_lesson_6.py
+++ homework_lesson_6.py
@@ -1,4 +1,5 @@
 import random
+
 # task_1
 my_list1 = [random.randint(1, 200) for i in range(20)]
 for index in range(len(my_list1)):
@@ -16,10 +17,11 @@ print(my_result)
 
 
 # task_3
-if len(my_list1) < 2:
-    my_list1.append(0)
-elif len(my_list1) >= 2:
-    my_list1.append(my_list1[-1]+my_list1[-2])
+my_list3 = [random.randint(1, 200) for i in range(20)]
+if len(my_list3) < 2:
+    my_list3.append(0)
+elif len(my_list3) >= 2:
+    my_list3.append(my_list3[-1] + my_list3[-2])
 else:
     pass
 
@@ -27,7 +29,6 @@ else:
 # task_4
 my_list4 = [random.randint(-100, 100) for i in range(20)]
 k = 3
-print(my_list4)
 for index in range(k - 1, len(my_list4)):
     if index < len(my_list4):
         my_list4[index - 1] = my_list4[index]
@@ -40,11 +41,11 @@ my_list4.pop()
 my_list5 = [random.randint(-100, 100) for i in range(20)]
 k = 5
 C = 7
-for index in range(len(my_list5)+1):
+for index in range(len(my_list5) + 1):
     if index == k:
         my_list5.append(0)
-        for j in range(len(my_list5)-1, k, -1):
-            my_list5[j] = my_list5[j-1]
+        for j in range(len(my_list5) - 1, k, -1):
+            my_list5[j] = my_list5[j - 1]
         my_list5[k] = C
     else:
         pass
@@ -53,4 +54,4 @@ for index in range(len(my_list5)+1):
 # task_6
 my_list6 = [random.randint(-100, 100) for i in range(20)]
 my_list7 = [random.randint(-100, 100) for i in range(20)]
-print('Uniq valuse count in my_list6 and my_list7: ', len(set(my_list1)) + len(set(my_list2)))
+print('Uniq valuse count in my_list6 and my_list7: ', len(set(my_list6)) + len(set(my_list7)))
