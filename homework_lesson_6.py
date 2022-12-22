3b5f630f96277db09250224f21a95277f2781a70
Andrew <andreym89@gmail.com>
2022-12-23 01:08:14 +0200

    Homework 5 task 1 - 6 done

--- homework_lesson_6.py
+++ homework_lesson_6.py
@@ -1,12 +1,11 @@
 import random
 # task_1
-'''
 my_list1 = [random.randint(1, 200) for i in range(20)]
 for index in range(len(my_list1)):
     if my_list1[index] > 100:
         print(my_list1[index], end=' ')
 print()
-#print(my_list)
+
 
 # task_2
 my_result = []
@@ -15,6 +14,7 @@ for index in range(len(my_list1)):
         my_result.insert(1, my_list1[index])
 print(my_result)
 
+
 # task_3
 if len(my_list1) < 2:
     my_list1.append(0)
@@ -22,12 +22,22 @@ elif len(my_list1) >= 2:
     my_list1.append(my_list1[-1]+my_list1[-2])
 else:
     pass
-#print(my_list)
-'''
+
+
 # task_4
+my_list4 = [random.randint(-100, 100) for i in range(20)]
+k = 3
+print(my_list4)
+for index in range(k - 1, len(my_list4)):
+    if index < len(my_list4):
+        my_list4[index - 1] = my_list4[index]
+    else:
+        break
+my_list4.pop()
+
+
 # task_5
 my_list5 = [random.randint(-100, 100) for i in range(20)]
-print(my_list5)
 k = 5
 C = 7
 for index in range(len(my_list5)+1):
@@ -38,11 +48,9 @@ for index in range(len(my_list5)+1):
         my_list5[k] = C
     else:
         pass
-print(my_list5)
 
-'''
+
 # task_6
 my_list6 = [random.randint(-100, 100) for i in range(20)]
 my_list7 = [random.randint(-100, 100) for i in range(20)]
 print('Uniq valuse count in my_list6 and my_list7: ', len(set(my_list1)) + len(set(my_list2)))
-'''
