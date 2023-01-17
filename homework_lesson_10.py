5548be6244c11a6ed0678178e61996ebe9344e63
Andrew <andreym89@gmail.com>
2023-01-17 20:10:48 +0200

    Homework 9 task 1 done

--- homework_lesson_10.py
+++ homework_lesson_10.py
@@ -1,53 +1,3 @@
-# Files
-'''
-with open('files/testFile.txt', 'wt') as file: # 'at' - добавить к концу файла еще данные если будут
-    for i in range(0, 4):
-        file.write('Hello world\n')
-
-li = ['123', 'qwe', '456']
-
-with open('files/testFile.txt', 'at') as file:
-    file.writelines([i+'\n' for i in li])
-
-
-with open('files/testFile.txt', 'rt') as file:
-    result = file.read()
-    print(result)
-
-
-with open('files/testFile.txt', 'rt') as file:
-    result = file.read(14)
-    print(result)
-
-
-with open('files/testFile.txt', 'rt') as file:
-    result = file.readline()
-    print(result)
-
-
-with open('files/testFile.txt', 'rt') as file:
-    result = file.readlines()
-    print(result)
-
-
-def read_txt_file(filename: str):
-    with open(filename, 'rt') as file:
-        data = file.read()
-        return data
-    return 0
-
-
-def write_txt_file(filename: str, data) -> None:
-    with open(filename, 'wt') as file:
-        data = file.write(str(data))
-
-
-text = read_txt_file('files/testFile.txt')
-if text != 0:
-    print(text)
-'''
-
-
 # Task_1
 
 def read_txt_file(filename: str):
@@ -61,18 +11,23 @@ def write_txt_file(filename: str, data) -> None:
     with open(filename, 'wt') as file:
         data = file.write(str(data))
 
-
+def word_cleaner(word: str) -> list:
+    list_word_spec_chair = []
+    return list_word_spec_chair
 def censor_function(forbidden_word: str) -> str:
     """
     функция возвращает строку с подмененными центральными символами на *
     :param forbidden_word: само слово
     :return: обработанное слово
     """
-
+    list = word_cleaner(forbidden_word)
     if len(forbidden_word) > 2:
         star_count = len(forbidden_word) - 2
         forbidden_word = forbidden_word[0] + '*' * star_count + forbidden_word[len(forbidden_word) - 1]
     return forbidden_word
+
+
+
 
 
 def censored_text(text: str, forbidden_words: list) -> None:
@@ -86,9 +41,10 @@ def censored_text(text: str, forbidden_words: list) -> None:
     source_text = read_txt_file(text)
     source_text_copy = source_text.split(sep=' ')
 
+    print(source_text_copy)
     for word in range(0, len(source_text_copy)):
         for f_word in range(0, len(forbidden_words)):
-            if source_text_copy[word].lower() == str(forbidden_words[f_word]):
+            if str(forbidden_words[f_word]) in source_text_copy[word].lower():
                 source_text_copy[word] = censor_function(source_text_copy[word])
 
     source_text_copy = ' '.join(source_text_copy)
