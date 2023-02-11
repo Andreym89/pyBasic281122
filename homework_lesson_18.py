b8cb02c5b52eaa62ddc42359a102f210cd7827cd
Andrew <andreym89@gmail.com>
2023-02-11 16:29:53 +0200

    Homework 18 - Task 1 Done

--- homework_lesson_18.py
+++ homework_lesson_18.py
@@ -1,7 +1,6 @@
 # Task 1
-
-def printGreen(skk): print("\033[92m {}\033[00m" .format(skk))
-def printYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def print_green(skk): print("\033[92m{}\033[00m".format(skk))
+def print_yellow(skk): print("\033[93m{}\033[00m".format(skk))
 
 
 class Vehicle:
@@ -20,8 +19,8 @@ class Vehicle:
         self.max_speed = max_speed
 
     def __str__(self):
-        return f'Id number: {self.id_number}\nWheels count: {self.wheels_count}\nEngine: {self.engine_power} hp\n' \
-               f'Max speed: {self.max_speed} km/h'
+        return f'Vehicle class:\nId number: {self.id_number}\nWheels count: {self.wheels_count}\n' \
+               f'Engine: {self.engine_power} hp\nMax speed: {self.max_speed} km/h'
 
 
 class MotorCar(Vehicle):
@@ -46,7 +45,7 @@ class MotorCar(Vehicle):
         self.max_speed = max_speed
 
     def __str__(self):
-        return f'Car name: {self.car_name}\nModel: {self.car_model}\nYear: {self.car_year_model}\n' \
+        return f'MotorCar class:\nCar name: {self.car_name}\nModel: {self.car_model}\nYear: {self.car_year_model}\n' \
                f'Color: {self.car_color}\nVin code: {self.car_vin_code}\n' \
                f'Number of seats: {self.car_number_of_seats}\nWheels count: {self.wheels_count}\n' \
                f'Engine: {self.engine_power} hp\nMax speed: {self.max_speed} km/h'
@@ -71,7 +70,7 @@ class Bus(Vehicle):
         self.bus_count_of_standing_places = standing_places
 
     def __str__(self):
-        return f'Car name: {self.car_name}\nModel: {self.car_model}\nYear: {self.car_year_model}\n' \
+        return f'Bus class:\nCar name: {self.car_name}\nModel: {self.car_model}\nYear: {self.car_year_model}\n' \
                f'Color: {self.car_color}\nVin code: {self.car_vin_code}\n' \
                f'Number of seats: {self.car_number_of_seats}\nWheels count: {self.wheels_count}\n' \
                f'Engine: {self.engine_power} hp\nMax speed: {self.max_speed} km/h\n' \
@@ -89,32 +88,71 @@ class Bicycle(Vehicle):
         self.stype = bicycle_type
 
     def __str__(self):
-        return f'Wheels count: {self.wheels_count}\nMax speed: {self.max_speed} km/h\n' \
+        return f'Bicycle class:\nWheels count: {self.wheels_count}\nMax speed: {self.max_speed} km/h\n' \
                f'Bicycle type: {self.stype}\nColor: {self.color}'
 
 
 class MotorBike(Bicycle):
-    pass
+    def __init__(self, wheels_count: int, max_speed: int, color: str, bicycle_type: str, id_num: str, power: int):
+        self.wheels_count = wheels_count
+        self.max_speed = max_speed
+        self.color = color
+        self.stype = bicycle_type
+        self.id_number = id_num
+        self.engine_power = power
+
+    def __str__(self):
+        return f'Id num: {self.id_number}\nPower: {self.engine_power}\nWheels count: {self.wheels_count}\n' \
+               f'Max speed: {self.max_speed} km/h\nBicycle type: {self.stype}\nColor: {self.color}'
 
 
 class Truck(MotorCar):
-    pass
+    trailer_type = str()
+    load_capacity = int()
+
+    def __init__(self, car_name: str, car_model: str, car_year_model: int, car_color: str,
+                 car_vin_code: str, car_number_of_seats: int, id_num: str, wheel_count: int,
+                 power: str, max_speed: int, trailer_type:str, load_cap: int):
+        self.car_name = car_name
+        self.car_model = car_model
+        self.car_year_model = car_year_model
+        self.car_color = car_color
+        self.car_vin_code = car_vin_code
+        self.car_number_of_seats = car_number_of_seats
+        self.id_number = id_num
+        self.wheels_count = wheel_count
+        self.engine_power = power
+        self.max_speed = max_speed
+        self.trailer_type = trailer_type
+        self.load_capacity = load_cap
+
+    def __str__(self):
+        return f'MotorCar class:\nCar name: {self.car_name}\nModel: {self.car_model}\nYear: {self.car_year_model}\n' \
+               f'Color: {self.car_color}\nVin code: {self.car_vin_code}\n' \
+               f'Number of seats: {self.car_number_of_seats}\nWheels count: {self.wheels_count}\n' \
+               f'Engine: {self.engine_power} hp\nMax speed: {self.max_speed} km/h\n' \
+               f'Trailer type: {self.trailer_type}\nLoad capacity: {self.load_capacity} kg'
 
 
-
-
-printGreen('Object of parent class')
+print_green('Object of parent class')
 new_car = Vehicle('AE0000TO', 4, 107, 180)
 print(new_car)
 
 my_auto = MotorCar('ZAZ', 'Lanos', 2011, 'Blue', 'VDBWDEVEW', 5, 'AE0000TO', 4, 105, 180)
-printYellow('\nMotorCar is child of class Vehicle')
+print_yellow('\nMotorCar is child of class Vehicle')
 print(my_auto)
 
-my_bus = Bus('ZAZ', 'Lanos', 2011, 'Blue', 'VDBWDEVEW', 18, 'AE0000TO', 4, 105, 180, 12)
-printYellow('\nBus is child of class Vehicle')
+my_bus = Bus('Mercedes', 'Sprinter', 2008, 'White', 'VDBWDEVEWASDFA', 18, 'AE0000TO', 4, 105, 180, 12)
+print_yellow('\nBus is child of class Vehicle')
 print(my_bus)
 
 my_bicycle = Bicycle(2, 35, 'Red', 'Mountain')
-printYellow('\nBicycle is child of class Vehicle')
+print_yellow('\nBicycle is child of class Vehicle')
 print(my_bicycle)
+
+my_truck = Truck('MAN', 'TGS NN', 2011, 'Grey', 'VDBWDEVEW', 2, 'AE0000TO', 4, 350, 150, "Trailer", 20000)
+print_yellow('\nTruck is child of class Vehicle')
+print(my_truck)
+
+
+# Task 2
